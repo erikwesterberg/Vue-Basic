ec6a7e66b511e29028d2605e6c4374c4e4da963a
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-18 17:19:27 +0200

    creates an array with three object with a todo, and store them in data inside of app component

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,33 @@ export default {
   name: 'app',
   components: {
  
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+      title: "Learn Vue and then look at Nuxt",
+      completed: false
+    },
+    {
+      id: 1,
+      title: "Enjoy it",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Drink beer",
+      completed: true
+    },
+    {
+      id: 3,
+      title: "Refactor code",
+      completed: false
+    }
+    
+      ]
+    }
   }
 }
 </script>
